098a03df55db7387d8b44649e538ad9b7f7d35bb
theplas <kanicepakulate@gmail.com>
2020-09-19 12:11:30 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace bai5{
+class Program
+{
+        private static int age;
+        
+
+        static void Main(string[] args)
+    {
+       
+        Console.WriteLine("Bạn có tuổi là {0}.",age);
+        Console.ReadLine();
+    }
+}
+}
